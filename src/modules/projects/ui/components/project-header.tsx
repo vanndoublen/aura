a60f47931f38c6a1ec067f2463a154c4e87b3e25
iamvann <sovannsean9@gmail.com>
2025-07-22 00:34:34 +0800

    add project header

--- src/modules/projects/ui/components/project-header.tsx
+++ src/modules/projects/ui/components/project-header.tsx
@@ -1,11 +1,53 @@
+import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
+import { ChevronLeft, Sidebar } from "lucide-react"
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { UserControl } from "@/components/user-control";
+
 interface Props {
-    
+    isOpen: boolean;
+    setIsOpen: (open: boolean) => void;
 }
 
-export const ProjectHeader = () => {
+export const ProjectHeader = ({ isOpen, setIsOpen }: Props) => {
     return (
-        <div>
-            
-        </div>
+        <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+            <div className="flex items-center">
+                <Button
+                    size="sm"
+                    variant="ghost"
+                    className="border-none"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <Sidebar />
+                </Button>
+                <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    className="h-8 border-none focus-visible:ring-0 hover:bg-transparent hover:opacity-75 transition-opacity pl-2!"
+                    asChild
+                >
+                    <Link href={`/`}>
+                        <ChevronLeft />
+
+                        <span className="text-sm font-medium text-[12px]">Home</span>
+                    </Link>
+                </Button>
+            </div>
+            <div className="flex items-center gap-x-4">
+
+
+
+                <UserControl />
+
+            </div>
+        </header>
+
     )
 }
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -20,152 +20,14 @@ interface Props {
     projectId: string;
 }
 
-const data = {
-    navMain: [
-        {
-            title: "Getting Started",
-            url: "#",
-            items: [
-                {
-                    title: "Installation",
-                    url: "#",
-                },
-                {
-                    title: "Project Structure",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "Building Your Application",
-            url: "#",
-            items: [
-                {
-                    title: "Routing",
-                    url: "#",
-                },
-                {
-                    title: "Data Fetching",
-                    url: "#",
-                    isActive: true,
-                },
-                {
-                    title: "Rendering",
-                    url: "#",
-                },
-                {
-                    title: "Caching",
-                    url: "#",
-                },
-                {
-                    title: "Styling",
-                    url: "#",
-                },
-                {
-                    title: "Optimizing",
-                    url: "#",
-                },
-                {
-                    title: "Configuring",
-                    url: "#",
-                },
-                {
-                    title: "Testing",
-                    url: "#",
-                },
-                {
-                    title: "Authentication",
-                    url: "#",
-                },
-                {
-                    title: "Deploying",
-                    url: "#",
-                },
-                {
-                    title: "Upgrading",
-                    url: "#",
-                },
-                {
-                    title: "Examples",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "API Reference",
-            url: "#",
-            items: [
-                {
-                    title: "Components",
-                    url: "#",
-                },
-                {
-                    title: "File Conventions",
-                    url: "#",
-                },
-                {
-                    title: "Functions",
-                    url: "#",
-                },
-                {
-                    title: "next.config.js Options",
-                    url: "#",
-                },
-                {
-                    title: "CLI",
-                    url: "#",
-                },
-                {
-                    title: "Edge Runtime",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "Architecture",
-            url: "#",
-            items: [
-                {
-                    title: "Accessibility",
-                    url: "#",
-                },
-                {
-                    title: "Fast Refresh",
-                    url: "#",
-                },
-                {
-                    title: "Next.js Compiler",
-                    url: "#",
-                },
-                {
-                    title: "Supported Browsers",
-                    url: "#",
-                },
-                {
-                    title: "Turbopack",
-                    url: "#",
-                },
-            ],
-        },
-        {
-            title: "Community",
-            url: "#",
-            items: [
-                {
-                    title: "Contribution Guide",
-                    url: "#",
-                },
-            ],
-        },
-    ],
-}
-
 export const ProjectView = ({ projectId }: Props) => {
     const { has } = useAuth();
     const hasProAccess = has?.({ plan: "pro" });
     const trpc = useTRPC();
     const { data: projects } = useSuspenseQuery(trpc.projects.getMany.queryOptions())
 
+
+    const [isOpen, setIsOpen] = useState(true); 
 
 
     return (
@@ -177,6 +39,8 @@ export const ProjectView = ({ projectId }: Props) => {
                         "--sidebar-width": "19rem",
                     } as React.CSSProperties
                 }
+                open={isOpen} 
+                onOpenChange={setIsOpen}
             >
                 <Sidebar variant="floating">
                     <SidebarContent>
@@ -212,13 +76,7 @@ export const ProjectView = ({ projectId }: Props) => {
 
 
                 <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
-                    </header>
+                    <ProjectHeader isOpen={isOpen} setIsOpen={setIsOpen}/>
 
                     {/* <ErrorBoundary fallback={<p>Messages container error</p>}> */}
                     <Suspense fallback={<p>loading messages. ... . </p>}>
