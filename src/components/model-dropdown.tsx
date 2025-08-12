809af83df92879cb690f008804358edfe4a38318
iamvann <sovannsean9@gmail.com>
2025-08-12 20:54:47 +0800

    add: search filter for model dropdown

--- src/components/model-dropdown.tsx
+++ src/components/model-dropdown.tsx
@@ -3,12 +3,13 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { type AiModel } from "@/generated/prisma";
 import { CheckIcon, ChevronDownIcon } from "lucide-react";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 
 interface Props {
@@ -18,6 +19,18 @@ interface Props {
 }
 
 export const ModelDropdown = ({ aiModels, selectedModel, setSelectedModel }: Props) => {
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredModels, setFilteredModels] = useState<AiModel[]>(aiModels);
+
+    const handleOnChangeSearch = (modelName: string) => {
+        setSearchValue(modelName);
+        if (modelName.length === 0) {
+            setFilteredModels(aiModels);
+        } else {
+            const result = aiModels.filter(model => model.displayName?.toLowerCase().includes(modelName.toLowerCase()))
+            setFilteredModels(result);
+        }
+    }
 
     return (
         <DropdownMenu>
@@ -34,9 +47,16 @@ export const ModelDropdown = ({ aiModels, selectedModel, setSelectedModel }: Pro
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-                <DropdownMenuLabel>Select AI Model</DropdownMenuLabel>
+                <Input
+                    placeholder="Search AI Models"
+                    value={searchValue}
+                    onChange={(e) => handleOnChangeSearch(e.target.value)}
+                    onKeyDown={(e) => e.stopPropagation()}
+                    className="!text-sm shadow-none !bg-transparent border-none focus-visible:border-transparent focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none"
+
+                />
                 <DropdownMenuSeparator />
-                {aiModels.map((model) => (
+                {filteredModels.map((model) => (
                     <DropdownMenuItem
                         key={model.id}
                         onSelect={() => setSelectedModel(model)}
@@ -48,6 +68,11 @@ export const ModelDropdown = ({ aiModels, selectedModel, setSelectedModel }: Pro
                         )}
                     </DropdownMenuItem>
                 ))}
+                {filteredModels.length === 0 && searchValue && (
+                    <div className="text-sm p-2 flex justify-center text-muted-foreground">
+                        No models found
+                    </div>
+                )}
             </DropdownMenuContent>
         </DropdownMenu>
     )
